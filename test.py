978636ee14815e8252e19b58fe6e8745bcb95708
Quinn McCarthy <quinnlanmccarthy@gmail.com>
2026-02-21 22:11:02 -0800

    oops

--- test.py
+++
@@ -1,8 +0,0 @@
-from gradio_client import Client, handle_file
-
-client = Client("Ateeqq/ai-image-detector")
-result = client.predict(
-		image_pil=handle_file('https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png'),
-		api_name="/predict"
-)
-print(result)
